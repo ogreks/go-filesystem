90bdc9d9383363dd99bf6c6a3c40a687c7a187b1
ogreks <ogreks@hotmail.com>
2023-09-11 12:02:49 +0800

    fix

--- qiniu/storage_test.go
+++ qiniu/storage_test.go
@@ -24,6 +24,7 @@ type MyPutRet struct {
 var (
 	accessKeyID     = os.Getenv("QINIU_OSS_ACCESSKEY_ID")
 	accessKeySecret = os.Getenv("QINIU_OSS_ACCESSKEY_SECRET")
+	bucketName      = os.Getenv("BUCKET")
 )
 
 type S Client
@@ -31,13 +32,12 @@ type S Client
 func TestStorage_PutFile2(t *testing.T) {
 
 	if accessKeyID == "" || accessKeySecret == "" {
-		t.Log("accessKeyID/accessKeySecret not found...")
+		t.Log("qiniu kodo configure not found...")
 		return
 	}
-	bucketFile := "go-file-system"
 	//上传凭据
 	putPolicy := storage.PutPolicy{
-		Scope: bucketFile,
+		Scope: bucketName,
 	}
 	mac := qbox.NewMac(accessKeyID, accessKeySecret)
 	upToken := putPolicy.UploadToken(mac)
@@ -83,7 +83,7 @@ func TestStorage_PutFile2(t *testing.T) {
 			},
 			after: func(t *testing.T, target string) {
 				require.NoError(t, os.Remove("test_put.txt"))
-				require.NoError(t, NewBucketManager.Delete(bucketFile, target))
+				require.NoError(t, NewBucketManager.Delete(bucketName, target))
 			},
 			target: "test_put.txt",
 			file: func(t *testing.T) fs.File {
@@ -108,94 +108,3 @@ func TestStorage_PutFile2(t *testing.T) {
 		})
 	}
 }
-
-//
-//func TestStorage_PutFile(t *testing.T) {
-//
-//	type fields struct {
-//		client  *storage.FormUploader
-//		upToken string
-//	}
-//	type args struct {
-//		ctx    context.Context
-//		target string
-//		file   string
-//	}
-//	tests := []struct {
-//		name    string
-//		fields  fields
-//		args    args
-//		wantErr bool
-//	}{
-//		// TODO: Add test cases.
-//		{
-//			name: "test1",
-//			fields: struct {
-//				client  *storage.FormUploader
-//				upToken string
-//			}{client: StorageFormUploader(), upToken: getToken()},
-//			args: struct {
-//				ctx    context.Context
-//				target string
-//				file   string
-//			}{ctx: context.Background(), target: "github-7.png", file: "../resources/github.png"},
-//		},
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			s := &Storage{
-//				client:  tt.fields.client,
-//				upToken: tt.fields.upToken,
-//			}
-//			if err := s.PutFile(tt.args.ctx, tt.args.target, tt.args.file); (err != nil) != tt.wantErr {
-//				t.Errorf("PutFile() error = %v, wantErr %v", err, tt.wantErr)
-//			}
-//		})
-//	}
-//}
-//
-//func getToken() string {
-//
-//	putPolicy := storage.PutPolicy{
-//		Scope: bucket,
-//	}
-//	mac := qbox.NewMac(accessKeyID, accessKeySecret)
-//	return putPolicy.UploadToken(mac)
-//}
-//func StorageFormUploader() *storage.FormUploader {
-//	cfg := storage.Config{}
-//	// 空间对应的机房
-//	cfg.Region = &storage.ZoneHuanan
-//	// 是否使用https域名
-//	cfg.UseHTTPS = true
-//	// 上传是否使用CDN上传加速
-//	cfg.UseCdnDomains = false
-//	return storage.NewFormUploader(&cfg)
-//
-//}
-//func Test(t *testing.T) {
-//
-//	localFile := "../resources/github.png"
-//	key := "github-6.png"
-//	putPolicy := storage.PutPolicy{
-//		Scope: bucket,
-//	}
-//	mac := qbox.NewMac(accessKey, secretKey)
-//	upToken := putPolicy.UploadToken(mac)
-//	cfg := storage.Config{}
-//	// 空间对应的机房
-//	cfg.Region = &storage.ZoneHuanan
-//	// 是否使用https域名
-//	cfg.UseHTTPS = true
-//	// 上传是否使用CDN上传加速
-//	cfg.UseCdnDomains = false
-//
-//	// 构建表单上传的对象
-//	formUploader := storage.NewFormUploader(&cfg)
-//	storage := NewFileSystem(formUploader, upToken)
-//	err := storage.PutFile(context.Background(), key, localFile)
-//	if err != nil {
-//		fmt.Print("上传失败：", err)
-//	}
-//	fmt.Print("上传成功！")
-//}
